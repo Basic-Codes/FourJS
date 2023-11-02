9c645a0c0d8afab58b56b874ef8b2dab0fcf0346
rifatNr <Rifatnoor92@gmail.com>
2023-11-02 13:46:15 +0600

    changes

--- web-app/tailwind.config.js
+++ web-app/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+            'vr-gray': {
+                50: '#F7F8F9',
+            }
+        },
+        fontFamily: {
+            roboto: ['Roboto Slab', 'sans-serif'],
+            inter: ['Inter', 'sans-serif'],
+        },
+    },
   },
   plugins: [],
 }
