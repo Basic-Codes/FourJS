eae985b23cfd9e9015ea070edd6afa1dc791e313
rifatNr <Rifatnoor92@gmail.com>
2023-12-12 05:22:48 +0600

    experimenting with threejs & vivox

---
+++ experiments/Three JS Eperiment/main.js
@@ -0,0 +1,30 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 2, 3 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+// const loader = new GLTFLoader();
+
+camera.position.z = 5;
+
+camera.position.set(0, 2, 5)
+controls.update()
+
+cube.rotation.x = 20
+cube.rotation.y = -20
+
+renderer.render( scene, camera );
+
+OrbitC
