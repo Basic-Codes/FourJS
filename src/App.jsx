e6af4a7c33f6879e4a77a96870f2fd0683b3750d
rifatNR <Rifatnoor92@gmail.com>
2024-01-14 14:50:11 +0600

    First Tutorial done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
+import MyBaseScene from "./components/MyBaseScene";
+import TutBaseScene from "./components/TutBaseScene";
 
 function App() {
-    return <div>Hello World</div>;
+    return (
+        <div>
+            {/* <MyBaseScene /> */}
+            <TutBaseScene />
+            {/* <div>Hello World</div> */}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/TutBaseScene.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import * as THREE from "three";
+import SceneInit from "../helper/SceneInit";
+
+function TutBaseScene() {
+    useEffect(() => {
+        const test = new SceneInit("myThreeJsCanvas");
+        test.initialize();
+        test.animate();
+
+        const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+        const boxMaterial = new THREE.MeshNormalMaterial();
+        const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+        test.scene.add(boxMesh);
+    }, []);
+
+    return (
+        <div>
+            <canvas id="myThreeJsCanvas" />
+        </div>
+    );
+}
+
+export default TutBaseScene;
