b0654398d33c4f08a1e36cbd3dc0c2527b416ec2
rifatNR <Rifatnoor92@gmail.com>
2024-02-28 17:07:45 +0600

    Added Teacher go to VR session button

--- frontend/src/components/Classroom/TeacherPanel.jsx
+++ frontend/src/components/Classroom/TeacherPanel.jsx
@@ -4,10 +4,11 @@ import Whiteboard from "./Whiteboard";
 import { useEffect } from "react";
 import { ref, onValue, set } from "firebase/database";
 import { db } from "../../helper/firebase";
-import { useParams } from "wouter";
+import { useLocation, useParams } from "wouter";
 
 const TeacherPanel = () => {
     const params = useParams();
+    const [location, setLocation] = useLocation();
 
     const [currState, setCurrState] = useState("whiteboard");
     const [currPptImgUrl, setCurrPptImgUrl] = useState(null);
@@ -32,21 +33,37 @@ const TeacherPanel = () => {
 
     return (
         <div>
-            <div
-                className="absolute left-1/2 top-0 -translate-x-1/2
-                        bg-white rounded-b-lg px-4 py-1
+            <div className="absolute left-1/2 top-0 -translate-x-1/2 flex space-x-5">
+                <div
+                    className="bg-white rounded-b-lg px-4 py-1
                         cursor-pointer font-semibold shadow"
-            >
-                {currState == "whiteboard" && (
-                    <button onClick={() => setCurrState("slide")}>
-                        Whiteboard Mode
+                >
+                    {currState == "whiteboard" && (
+                        <button onClick={() => setCurrState("slide")}>
+                            Whiteboard Mode
+                        </button>
+                    )}
+                    {currState == "slide" && (
+                        <button onClick={() => setCurrState("whiteboard")}>
+                            Slide Mode
+                        </button>
+                    )}
+                </div>
+                <div
+                    className="bg-white rounded-b-lg px-4 py-1
+                        cursor-pointer font-semibold shadow"
+                >
+                    <button
+                        onClick={() =>
+                            window.open(
+                                `/session/${params?.session_code}/student/teacher?isTeacher`,
+                                "_blank"
+                            )
+                        }
+                    >
+                        Go to VR Mode
                     </button>
-                )}
-                {currState == "slide" && (
-                    <button onClick={() => setCurrState("whiteboard")}>
-                        Slide Mode
-                    </button>
-                )}
+                </div>
             </div>
             {currState == "whiteboard" ? (
                 <Whiteboard />
